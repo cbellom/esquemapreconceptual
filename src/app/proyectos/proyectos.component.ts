e7436fb3fcbd6234135179b1fc72e6c77e2a952a
Camilo García <gcgarciab@slabcode.com>
2019-03-05 09:55:45 -0500

    Algunos cambios

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ProyectoDataService} from '../servicios/proyecto-data.service';
 import {Proyecto} from '../modelos/proyecto';
+import {MiembrosDataService} from '../servicios/miembros-data.service';
+import {Miembro} from '../modelos/miembro';
 
 @Component({
   selector: 'app-proyectos',
@@ -9,11 +11,14 @@ import {Proyecto} from '../modelos/proyecto';
 })
 export class ProyectosComponent implements OnInit {
 
-  constructor(public proyectoDataService: ProyectoDataService) { }
+  constructor(public proyectoDataService: ProyectoDataService, public miembrosDataService: MiembrosDataService) { }
 
   projects: Proyecto[] = this.proyectoDataService.datos;
+  creators: string[] = this.projects.map(value => {
+    return this.miembrosDataService.datos.find(value1 => value.creador === value1.id).nombre;
+  });
 
   ngOnInit() {
+    console.log(this.creators);
   }
-
 }
